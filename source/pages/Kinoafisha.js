34f63f13552bb719e7a059f353bca9d25a8bc7cd
Pavel Baranchuk <pbaranchuk@gmail.com>
2018-11-04 00:18:14 +0200

    Fix footer

--- source/pages/Kinoafisha.js
+++ source/pages/Kinoafisha.js
@@ -1,100 +1,115 @@
 // Core
-import React, { Component } from 'react';
-import cx from 'classnames';
+import React, { Component } from "react";
+import cx from "classnames";
 
 // Helpers
-import { getStyles } from '../helpers';
+import { getStyles } from "../helpers";
 
 // Api
-import { api } from '../API';
+import { api } from "../API";
 
 export class Kinoafisha extends Component {
-    state = {
-        selectedFilter: 'upcoming',
-        selectedMovie:  '',
-        movies:         [],
+  state = {
+    selectedFilter: "upcoming",
+    selectedMovie: "",
+    movies: []
+  };
+
+  componentDidMount() {
+    this._getMoviesByFilter(this.state.selectedFilter);
+  }
+
+  _getMoviesByFilter = async nextFilter => {
+    const movies = await api.getMovies(nextFilter);
+
+    this.setState({
+      movies
+    });
+  };
+
+  _selectFilter = event => {
+    const nextFilter = event.currentTarget.dataset.name;
+
+    this.setState({
+      selectedFilter: nextFilter
+    });
+
+    this._getMoviesByFilter(nextFilter);
+  };
+
+  _selectMovie = movieId => {
+    return () => {
+      this.setState({
+        selectedMovie: movieId
+      });
     };
+  };
 
-    componentDidMount() {
-        this._getMoviesByFilter(this.state.selectedFilter);
-    }
+  render() {
+    const styles = getStyles(this.state);
 
-    _getMoviesByFilter = async (nextFilter) => {
-        const movies = await api.getMovies(nextFilter);
+    const moviesJSX = this.state.movies.map(movie => {
+      const posterStyle = cx("poster", {
+        selectedPoster: movie.id === this.state.selectedMovie
+      });
 
-        this.setState({
-            movies,
-        });
-    };
+      return (
+        <div
+          className="movie"
+          key={movie.id}
+          onClick={this._selectMovie(movie.id)}
+        >
+          <div className={posterStyle}>
+            <span className="genre">{movie.genre}</span>
+            <img src={movie.poster} />
+            <span className="rating">{movie.rating}</span>
+          </div>
+          <span className="title">{movie.title}</span>
+        </div>
+      );
+    });
 
-    _selectFilter = (event) => {
-        const nextFilter = event.currentTarget.dataset.name;
-
-        this.setState({
-            selectedFilter: nextFilter,
-        });
-
-        this._getMoviesByFilter(nextFilter);
-    };
-
-    _selectMovie = (movieId) => {
-        return () => {
-            this.setState({
-                selectedMovie: movieId,
-            });
-        };
-    };
-
-    render() {
-        const styles = getStyles(this.state);
-
-        const moviesJSX = this.state.movies.map((movie) => {
-            const posterStyle = cx('poster', {
-                selectedPoster: movie.id === this.state.selectedMovie,
-            });
-
-            return (
-                <div
-                    className = 'movie'
-                    key = { movie.id }
-                    onClick = { this._selectMovie(movie.id) }>
-                    <div className = { posterStyle }>
-                        <span className = 'genre'>{movie.genre}</span>
-                        <img src = { movie.poster } />
-                        <span className = 'rating'>{movie.rating}</span>
-                    </div>
-                    <span className = 'title'>{movie.title}</span>
-                </div>
-            );
-        });
-
-        return (
-            <>
-                <div className = 'header'>
-                    <div className = 'logo' />
-                    <div className = 'filters'>
-                        <div
-                            className = { styles.latestFilter }
-                            data-name = 'latest'
-                            onClick = { this._selectFilter }>
-                            <span>Новинки 2018</span>
-                        </div>
-                        <div
-                            className = { styles.upcomingFilter }
-                            data-name = 'upcoming'
-                            onClick = { this._selectFilter }>
-                            <span>Скоро в кинотеатрах</span>
-                        </div>
-                        <div
-                            className = { styles.popularFilter }
-                            data-name = 'popular'
-                            onClick = { this._selectFilter }>
-                            <span>В топ-чартах</span>
-                        </div>
-                    </div>
-                </div>
-                <div className = 'content'>{moviesJSX}</div>
-            </>
-        );
-    }
+    return (
+      <>
+        <div className="header">
+          <div className="logo" />
+          <div className="filters">
+            <div
+              className={styles.latestFilter}
+              data-name="latest"
+              onClick={this._selectFilter}
+            >
+              <span>Новинки 2018</span>
+            </div>
+            <div
+              className={styles.upcomingFilter}
+              data-name="upcoming"
+              onClick={this._selectFilter}
+            >
+              <span>Скоро в кинотеатрах</span>
+            </div>
+            <div
+              className={styles.popularFilter}
+              data-name="popular"
+              onClick={this._selectFilter}
+            >
+              <span>В топ-чартах</span>
+            </div>
+          </div>
+        </div>
+        <div className="content">{moviesJSX}</div>
+        <div className="footer">
+          <a href="mailto:team@lectrum.io">tesm@lectrum.io</a>
+          <span>
+            2018 © Все права защищены. Разработано с любовью &nbsp;
+            <a href="https://lectrum.io/intensive/react">в Лектруме</a>
+          </span>
+          <div className="social">
+            <a class="facebook" href="https://www.facebook.com/lectrum/" />
+            <a class="telegram" href="https://t.me/lectrum" />
+          </div>
+        </div>
+      </>
+    );
+  }
 }
